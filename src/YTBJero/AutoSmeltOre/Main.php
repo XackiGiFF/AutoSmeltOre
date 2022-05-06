f999c6fbcfb44e5ce46534747d14d715d76d3db1
Ngọc Lam <nglamvn2911@gmail.com>
2022-05-06 13:49:06 +0700

    Move...

--- src/YTBJero/AutoSmeltOre/Main.php
+++ src/YTBJero/AutoSmeltOre/Main.php
@@ -5,23 +5,36 @@ namespace YTBJero\AutoSmeltOre;
 
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\item\enchantment\VanillaEnchantments;
+use pocketmine\item\Item;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\item\ItemIds;
 use pocketmine\item\StringToItemParser;
 
 class Main extends PluginBase implements Listener{
+	  /** @var Item[] */
+	  protected array $ores;
 
       public function onEnable() : void{
             $this->getServer()->getPluginManager()->registerEvents($this, $this);
             $this->saveDefaultConfig();
+			 $this->ores = [
+			  	  ItemIds::COAL_ORE => StringToItemParser::getInstance()->parse("coal"),
+			  	  ItemIds::IRON_ORE => StringToItemParser::getInstance()->parse("iron_ingot"),
+			  	  ItemIds::GOLD_ORE => StringToItemParser::getInstance()->parse("gold_ingot"),
+				  ItemIds::REDSTONE_ORE => StringToItemParser::getInstance()->parse("redstone"),
+			      ItemIds::LIT_REDSTONE_ORE => StringToItemParser::getInstance()->parse("redstone"),
+			  	  ItemIds::DIAMOND_ORE => StringToItemParser::getInstance()->parse("diamond"),
+			  	  ItemIds::EMERALD_ORE => StringToItemParser::getInstance()->parse("emerald"),
+				  ItemIds::QUARTZ_ORE => StringToItemParser::getInstance()->parse("quartz")
+			 ];
       }
 
-	 /**
-	  * @param BlockBreakEvent $event
-	  * @priority NORMAL
-	  * @handleCancelled FALSE
-	  */
+	  /**
+	   * @param BlockBreakEvent $event
+	   * @priority NORMAL
+	   * @handleCancelled FALSE
+	   */
       public function onBreak(BlockBreakEvent $event) : void{
             $item = $event->getItem();
             $block = $event->getBlock();
@@ -30,18 +43,9 @@ class Main extends PluginBase implements Listener{
                   return;
             }
             if($this->getConfig()->get("Permission", true) and $player->hasPermission("autosmeltore.action.allow")){
-                  $ores = [
-                        ItemIds::COAL_ORE => StringToItemParser::getInstance()->parse("coal"),
-                        ItemIds::IRON_ORE => StringToItemParser::getInstance()->parse("iron_ingot"),
-                        ItemIds::GOLD_ORE => StringToItemParser::getInstance()->parse("gold_ingot"),
-                        ItemIds::REDSTONE_ORE => StringToItemParser::getInstance()->parse("redstone"),
-                        ItemIds::LIT_REDSTONE_ORE => StringToItemParser::getInstance()->parse("redstone"),
-                        ItemIds::DIAMOND_ORE => StringToItemParser::getInstance()->parse("diamond"),
-                        ItemIds::EMERALD_ORE => StringToItemParser::getInstance()->parse("emerald"),
-                        ItemIds::QUARTZ_ORE => StringToItemParser::getInstance()->parse("quartz")
-                  ];
+
                   if($this->getConfig()->get(str_replace(" ", "_", $block->getName()))){
-						$drops = array_map(fn($item) => in_array($item->getId(), array_keys($ores)) ? $ores[$item->getId()] : $item, $event->getDrops());
+						$drops = array_map(fn($item) => in_array($item->getId(), array_keys($this->ores)) ? $this->ores[$item->getId()] : $item, $event->getDrops());
 						$event->setDrops($drops);
                   }
             }
